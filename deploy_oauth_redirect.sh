740dce724e3b4fd69ba0d82618f6daf42c0a3520
Rebekka Orth <>
2022-04-17 11:54:41 +0200

    add client id and secret to oauth deployment

--- deploy_oauth_redirect.sh
+++ deploy_oauth_redirect.sh
@@ -5,6 +5,16 @@ if [ -z ${SLACK_SIGNING_SECRET+x} ]; then
   exit 1
 fi
 
+if [ -z ${SLACK_CLIENT_ID+x} ]; then
+  echo "SLACK_CLIENT_ID is not set as environment variable"
+  exit 1
+fi
+
+if [ -z ${SLACK_CLIENT_SECRET+x} ]; then
+  echo "SLACK_CLIENT_SECRET is not set as environment variable"
+  exit 1
+fi
+
 if [ -z ${SLACK_BOT_TOKEN+x} ]; then
   echo "SLACK_BOT_TOKEN is not set as environment variable"
   exit 1
@@ -26,6 +36,8 @@ gcloud functions deploy oauth_redirect \
 --runtime python39 \
 --trigger-http \
 --set-env-vars "SLACK_SIGNING_SECRET=${SLACK_SIGNING_SECRET}" \
+--set-env-vars "SLACK_CLIENT_ID=${SLACK_CLIENT_ID}" \
+--set-env-vars "SLACK_CLIENT_SECRET=${SLACK_CLIENT_SECRET}" \
 --set-env-vars "SLACK_BOT_TOKEN=${SLACK_BOT_TOKEN}" \
 --set-env-vars "GOOGLE_CLOUD_PROJECT=${GOOGLE_CLOUD_PROJECT}" \
 --set-env-vars "ENCRYPTION_SECRET=${ENCRYPTION_SECRET}" \
