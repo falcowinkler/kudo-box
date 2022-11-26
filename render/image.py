d024bdcb70b36fdda104d1294407ce66f6118bd9
falcowinkler <falcowinkler@icloud.com>
2022-11-26 19:00:20 +0100

    Create font that allows emojis

--- render/image.py
+++ render/image.py
@@ -5,20 +5,19 @@ import uuid
 from PIL import Image, ImageFont, ImageDraw
 
 NUM_IMAGES = 11
-NUMBER_OF_CHARACTERS_BEFORE_CARRIER_RETURN = 25
+NUMBER_OF_CHARACTERS_BEFORE_CARRIER_RETURN = 20
 FONT_SIZE = 28
 X_ORIGIN_TEXT = 55
-Y_ORIGIN_TEXT = 148
+Y_ORIGIN_TEXT = 130
 VERTICAL_LINE_PADDING = 16
-TEXT_HEIGHT = 22
+TEXT_HEIGHT = 23
 MAXIMUM_NUMBER_OF_CHARACTERS = 224
 
 
 def create_card(text):
     text = f"""{text}"""
     message = (text[:MAXIMUM_NUMBER_OF_CHARACTERS] + ' ...') if len(text) > MAXIMUM_NUMBER_OF_CHARACTERS else text
-
-    font_path = 'fonts/MostlyMono.ttf'
+    font_path = 'fonts/NotoEmoji+MostlyMono.ttf'
     image = random.randint(1, NUM_IMAGES)
     x = Image.open(f'images/Group {image}.png').convert('RGB')
     lines = textwrap.wrap(message, width=NUMBER_OF_CHARACTERS_BEFORE_CARRIER_RETURN)
