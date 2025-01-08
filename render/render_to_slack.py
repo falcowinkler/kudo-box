8a80fa6696706f328690246052eda769959fe083
Falco Winkler <8613031+falcowinkler@users.noreply.github.com>
2025-01-08 09:06:59 +0100

    simplify mentions

--- render/render_to_slack.py
+++ render/render_to_slack.py
@@ -18,37 +18,18 @@ def post_initial_message(channel, credentials, text):
 
 
 def render_and_upload_kudo(channel, text, credentials, thread_ts=None):
-    mentioned = ", ".join(extract_mentions(text))
+    mentions = extract_mentions(text)
+    mentioned = "\n" + ", ".join(mentions)
     file_path = create_card(readable_mentions(text))
     slack_client = WebClient(token=credentials.bot_token)
     try:
         slack_client.conversations_join(channel=channel)
     except:  # already in channel
         pass
-    response = slack_client.files_upload_v2(
+    slack_client.files_upload_v2(
         channel=channel,
         file=file_path,
         thread_ts=thread_ts,
         filename="kudos.gif",
-        initial_comment=random_comment(),
+        initial_comment=random_comment() + mentioned if mentions else "",
         title=random_title())
-
-    def extract_ts(lst):
-        return [info["ts"] for info in lst]
-
-    def extract_shares(dct):
-        return [t for lst in dct.values() for t in extract_ts(lst)]
-
-    private_shares = extract_shares(response["file"]["shares"].get("private", {}))
-    public_shares = extract_shares(response["file"]["shares"].get("public", {}))
-    shares = private_shares + public_shares
-    if mentioned:
-        for thread_ts in shares:
-            slack_client.api_call(
-                'chat.postMessage',
-                data={
-                    'channel': channel,
-                    'thread_ts': thread_ts,
-                    'text': mentioned
-                }
-            )
